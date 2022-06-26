ac04940389aeb6b1460ad9ff13a4bdd06e614d2e
Vasyl Horeiko <vasyl.horeiko@signet.tv>
2022-06-26 14:17:43 +0300

    added mount binding for backend-app (does not work live update via nodemon in windows)

--- multi-container-app/backend/app.js
+++ multi-container-app/backend/app.js
@@ -95,7 +95,7 @@ mongoose.connect(
       console.error('FAILED TO CONNECT TO MONGODB');
       console.error(err);
     } else {
-      console.log('CONNECTED TO MONGODB');
+      console.log('CONNECTED TO MONGODB!!');
       console.log('process.env.PORT:', process.env.PORT);
       app.listen(process.env.PORT || 4001);
     }
